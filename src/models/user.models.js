e2d1c9748236ec3fe1878b03693fb38613549046
Mehdiali Kadiwala <mehdialikadiwala@gmail.com>
2025-02-14 16:47:53 +0530

    All models are created.

---
+++ src/models/user.models.js
@@ -0,0 +1,50 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+    {
+        userName: {
+            type: String,
+            required: true,
+            lowercase: true,
+            unique: true,
+            trim: true,
+            index: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            lowercase: true,
+            unique: true,
+            trim: true,
+        },
+        fullName: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true,
+        },
+        avatar: {
+            type: String, // Cloudinary
+            required: true,
+        },
+        coverImage: {
+            type: String, // Cloudinary
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video",
+            },
+        ],
+        password: {
+            type: String,
+            required: [true, "Password is required."],
+        },
+        refreshToken: {
+            type: String,
+        },
+    },
+    { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
