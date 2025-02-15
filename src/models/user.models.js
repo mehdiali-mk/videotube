feb296d8741c5849cefe4ece919536f64fee1986
Mehdiali Kadiwala <mehdialikadiwala@gmail.com>
2025-02-15 15:25:36 +0530

    Cloudinary upload setup.

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
     {
@@ -46,5 +48,45 @@ const userSchema = new Schema(
     },
     { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        next();
+    } else {
+        this.password = bcrypt.hash(this.password, 10);
+        next();
+    }
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = async function () {
+    return await jwt.sign(
+        {
+            _id: this._id,
+            userName: this.userName,
+            fullName: this.fullName,
+            email: this.email,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+        }
+    );
+};
+
+userSchema.methods.generateRefreshToken = async function () {
+    return await jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+        }
+    );
+};
 
 export const User = mongoose.model("User", userSchema);
---
+++ src/utils/cloudinary.utils.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// Configure the cloudinary
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME
+    api_key: process.env.CLOUD_API_KEY
+    api_secret: process.env.CLOUD_API_SECRET
+});
+
+const uploadOnCloudinary = async function(localFilePath) {
+    try {
+        if (!localFilePath) return null
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto",
+        });
+
+        console.log("File uploaded on cloudianry.\nURL: ", response.url);
+
+        fs.unlinkSync(localFilePath);
+
+        return response;
+    } catch(error) {
+        fs.unlinkSync(localFilePath);
+        return null;
+    }
+}
